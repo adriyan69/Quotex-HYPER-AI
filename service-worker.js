2324eefe31759baa5b018aa4260395c964366a47
adriyan69 <projectsupraa3@gmail.com>
2026-09-10 22:22:23 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'market-ai-shell-v1';
+const CACHE_NAME = 'market-ai-shell-v2';
 
 const SHELL_ASSETS = [
   './',
@@ -8,6 +8,9 @@ const SHELL_ASSETS = [
   './js/app.js',
   './js/core/MarketDataProvider.js',
   './js/core/MockDataProvider.js',
+  './js/core/ReplayDataProvider.js',
+  './js/core/CandleNormalizer.js',
+  './js/core/sampleData.js',
   './js/worker/engine.worker.js',
   './icons/icon-192.png',
   './icons/icon-512.png',
